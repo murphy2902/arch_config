4ed73065d0bf5f822440e929e6487d29b28ee2ae
Ryan Murphy <murphy2902@gmail.com>
2015-12-26 19:57:00 -0500

    Adding a colorscheme file

---
+++ .config/x/haruko.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Black + DarkGrey
+COLOR_0="#141618"
+COLOR_8="#40464b"
+# DarkRed + Red
+COLOR_1="#9b6465"
+COLOR_9="#de8f91"
+# DarkGreen + Green
+COLOR_2="#51ca7e"
+COLOR_10="#79de9e"
+# DarkYellow + Yellow
+COLOR_3="#eda757"
+COLOR_11="#fbd25d"
+# DarkBlue + Blue
+COLOR_4="#4e9ab7"
+COLOR_12="#65c8ed"
+# DarkMagenta + Magenta
+COLOR_5="#9b6481"
+COLOR_13="#ffb2bd"
+# DarkCyan + Cyan
+COLOR_6="#58488a"
+COLOR_14="#eae9da"
+# Light Grey + White
+COLOR_7="#bab9ad"
+COLOR_15="#eae9da"
+# Include, StatusLine, ErrorMsg
+COLOR_BD="#ffffff"
+COLOR_IT="#fbd25d"
+COLOR_UL="#de8f91"
+# Foreground, Background, Cursor
+COLOR_FORE="#eae9da"
+COLOR_BACK="#30363b"
+COLOR_CURS="#eae9da"
