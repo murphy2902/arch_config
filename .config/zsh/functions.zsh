9f9ecf2ece42bd09899420b58c254574730b6592
Ryan Murphy <murphy2902@gmail.com>
2015-12-29 11:38:38 -0500

    Added a specific hotkey to chromium changed cdl

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -2,4 +2,4 @@
 goto() { [ -d "$1" ] && cd "$1" || cd "$(dirname "$1")"; }
 cpf() { cp "$@" && goto "$_"; }
 mvf() { mv "$@" && goto "$_"; }
-cdl() { cd $@; pwd; ls -1; }
+cdl() { cd $@ && pwd && ls -1; }
