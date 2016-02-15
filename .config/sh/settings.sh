b21e555d2ede10a6595ed111d8c715aef8256895
Ryan Murphy <murphy2902@gmail.com>
2016-02-15 18:58:44 -0500

    Further tmux edits

--- .config/sh/settings.sh
+++ .config/sh/settings.sh
@@ -2,6 +2,8 @@ HISTFILE=~/.cache/zsh/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
 bindkey -v
+
+set -o emacs
 # End of lines configured by zsh-newuser-install
 # The following lines were added by compinstall
 zstyle :compinstall filename '/home/ryan/.zshrc'
