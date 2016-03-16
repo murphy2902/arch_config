2c39640f99cd5c570f9d568cb3643e784ef4f11c
Ryan Murphy <murphy2902@gmail.com>
2016-03-16 14:40:08 -0400

    Asdf

--- .config/sh/functions.sh
+++
@@ -1,9 +0,0 @@
-# Follow functions
-goto() { [ -d "$1" ] && cd "$1" || cd "$(dirname "$1")"; }
-cpf() { cp "$@" && goto "$_"; }
-mvf() { mv "$@" && goto "$_"; }
-cdl() { cd $@ && pwd && ls -1; }
-
-connect() { sudo ip link set ${WIFI_DEVICE} \
-	&& sudo wpa_supplicant -B -i ${WIFI_DEVICE} -c /etc/wpa_supplicant/wpa_supplicant.conf \
-	&& sudo dhcpcd ${WIFI_DEVICE} }
